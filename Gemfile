b2580c1548b4cf60fe3fac744610e686c6d45e44
Carl Henderson <chuckhendo@gmail.com>
2014-05-19 16:51:59 -0400

    Actually gathering information from PSD

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 ruby "2.0.0"
 gem 'sinatra', '1.4.5'
+gem 'psd'
--- web.rb
+++ web.rb
@@ -1,16 +1,78 @@
 require 'sinatra'
+require 'psd'
+require 'json'
 
 set :public_folder, 'public'
 
 get "/" do
-  send_file File.join(settings.public_folder, 'index.html')
+	send_file File.join(settings.public_folder, 'index.html')
 end
 
 post "/api" do
 
-  File.open('tmp/' + params['upload'][:filename], "w") do |f|
-    f.write(params['upload'][:tempfile].read)
-  end
-  return "The file was successfully uploaded!"
+	file_name = 'tmp/' + params['upload'][:filename]
 
+	File.open(file_name, "w") do |f|
+		f.write(params['upload'][:tempfile].read)
+	end
+
+
+	psd = PSD.new(file_name)
+
+	psd.parse!
+
+	psdinfo = psd.tree.descendant_layers
+	returnArray = []
+	psdinfo.each do |item|
+		if item.type && item.type.engine_data
+
+			data = item.type.engine_data
+			text = data.EngineDict.Editor.Text
+			runLengths = data.EngineDict.StyleRun.RunLengthArray
+			styleRuns = data.EngineDict.StyleRun.RunArray
+			fonts = data.ResourceDict.FontSet
+			prevRunLength = 0
+			runs = []
+			runLengths.each_with_index { |runLength, index|
+
+				run = { 
+					"text" => text[prevRunLength..prevRunLength + runLength - 1].strip,
+					"font-family" => fonts[styleRuns[index].StyleSheet.StyleSheetData.Font].Name,
+					"color" => colorToHex(styleRuns[index].StyleSheet.StyleSheetData.FillColor.Values),
+					"font-size" => "#{styleRuns[index].StyleSheet.StyleSheetData.FontSize.to_int}px"
+				}
+
+				runs.push(run)
+
+
+				prevRunLength += runLength
+			}
+
+			returnItem = {
+				"layer" => {"width" => item.layer.width, "height" => item.layer.height, "top" => item.layer.top, "left" => item.layer.left },
+				"text" => runs
+			}
+
+
+			returnArray.push(returnItem)
+
+		end
+	end
+
+	# psd.tree.save_as_png('tmp/renders/' + 'tmp.png')
+
+	content_type :json
+	return returnArray.to_json
+
+end
+
+
+def colorToHex(color)
+	hex = ''
+	color[1..3].each do |part|
+		num = part * 256
+		colorHex = "%x" % num
+		hex = hex + colorHex 
+	end
+	return "##{hex}"
 end
