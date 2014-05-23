e42bc406ccb4a4a56cc2a02ed96febd87c3ece24
Carl Henderson <chuckhendo@gmail.com>
2014-05-22 20:57:22 -0400

    Clean up of code

---
+++ lib/PSD_Text.rb
@@ -0,0 +1,101 @@
+# encoding: utf-8
+
+
+require 'psd'
+require 'json'
+require 'securerandom'
+
+
+
+PSD_Text = Class.new do
+	def initialize(file_name)
+		@psd = PSD.new(file_name)
+		@psd.parse!
+		@psd_tree = @psd.tree
+	end
+	
+	def create_render
+		# create tmp directory if it doesn't already exist
+		Dir.mkdir 'tmp' unless File.directory?('tmp')
+		
+		# generate a random filename for our render, and save it to disk
+		render_file_name = SecureRandom.urlsafe_base64(30) + '.png'
+		@psd.image.save_as_png('tmp/renders-' + render_file_name)
+		return render_file_name
+	end
+	
+	def get_text
+		text_layers = []
+		
+		parse_layer(@psd_tree) { |layer| text_layers << layer }
+
+		return text_layers
+		
+	end
+	
+private
+	def parse_layer(layer, &block)
+		# If this layer is a group of layers and is visible, recall this function with each child
+		if layer.has_children? && (layer.root? || layer.visible)
+			layer.children.each do |sublayer|
+				parse_layer(sublayer, &block)
+			end
+		# If this layer is visible and has text, return the layer data
+		elsif layer.type && layer.type.engine_data && layer.visible
+			text_data = layer.type.engine_data
+
+			text = text_data.EngineDict.Editor.Text
+			
+			runLengths = text_data.EngineDict.StyleRun.RunLengthArray
+			styleRuns = text_data.EngineDict.StyleRun.RunArray
+			fonts = text_data.ResourceDict.FontSet
+			prevRunLength = 0
+			runs = []
+			runLengths.each_with_index { |runLength, index|
+
+				run = { 
+					"text" => text[prevRunLength..prevRunLength + runLength - 1],
+					"font_info" => {
+						"font-family" => fonts[styleRuns[index].StyleSheet.StyleSheetData.Font].Name,
+						"color" => color_to_hex(styleRuns[index].StyleSheet.StyleSheetData.FillColor.Values),
+						"font-size" => "#{styleRuns[index].StyleSheet.StyleSheetData.FontSize.to_int}px"
+					}
+				}
+
+
+				if runs.length > 0 && runs[-1].has_key?("font_info") && run["font_info"] != runs[-1]["font_info"]
+					runs.push(run)
+					runs[-1]["text"].strip!
+				elsif runs.length == 0
+					runs.push(run)
+				else 
+					runs[-1]["text"] += run["text"]
+				end
+
+				prevRunLength += runLength
+			}
+
+			layer_data = {
+				"width" => layer.width, 
+				"height" => layer.height, 
+				"top" => layer.top, 
+				"left" => layer.left,
+				"text" => runs
+			}
+		
+			block.call(layer_data)
+
+		end
+	end
+	
+	def color_to_hex(color)
+		hex = ''
+		color[1..3].each do |part|
+			num = part * 256
+			colorHex = "%x" % num
+			hex = hex + colorHex 
+		end
+		return "##{hex}"
+	end
+end
+
--- web.rb
+++ web.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'psd'
 require 'json'
 require 'securerandom'
+require_relative 'lib/PSD_Text'
 
 
 set :public_folder, 'public'
@@ -11,6 +12,7 @@ get "/" do
 end
 
 post "/api" do
+	content_type :json
 
 	file_name = 'tmp/' + params['file'][:filename]
 
@@ -18,77 +20,16 @@ post "/api" do
 		f.write(params['file'][:tempfile].read)
 	end
 
-	render_file_name = SecureRandom.urlsafe_base64(30) + '.png'
+	all_psd = PSD_Text.new(file_name)
 
-	psd = PSD.new(file_name)
+	return {
+		image: all_psd.create_render,
+		layers: all_psd.get_text
+	}.to_json
 
-	psd.parse!
-
-	psdinfo = psd.tree.descendant_layers
-
-	returnArray = {
-		"image" => render_file_name,
-		"layers" => []
-	}
-
-	psdinfo.each do |item|
-		if item.type && item.type.engine_data
-
-			data = item.type.engine_data
-			text = data.EngineDict.Editor.Text
-			runLengths = data.EngineDict.StyleRun.RunLengthArray
-			styleRuns = data.EngineDict.StyleRun.RunArray
-			fonts = data.ResourceDict.FontSet
-			prevRunLength = 0
-			runs = []
-			runLengths.each_with_index { |runLength, index|
-
-				run = { 
-					"text" => text[prevRunLength..prevRunLength + runLength - 1].strip,
-					"font-family" => fonts[styleRuns[index].StyleSheet.StyleSheetData.Font].Name,
-					"color" => colorToHex(styleRuns[index].StyleSheet.StyleSheetData.FillColor.Values),
-					"font-size" => "#{styleRuns[index].StyleSheet.StyleSheetData.FontSize.to_int}px"
-				}
-
-				runs.push(run)
-
-
-				prevRunLength += runLength
-			}
-
-			returnItem = {
-				"width" => item.layer.width, 
-				"height" => item.layer.height, 
-				"top" => item.layer.top, 
-				"left" => item.layer.left,
-				"text" => runs
-			}
-
-
-			returnArray["layers"].push(returnItem)
-
-		end
-	end
-
-
-	psd.image.save_as_png('tmp/renders-' + render_file_name)
-
-	content_type :json
-	return returnArray.to_json
 
 end
 
 get '/renders/:render_file_name' do 
 	send_file File.join("tmp/renders-#{params[:render_file_name]}")
-end
-
-
-def colorToHex(color)
-	hex = ''
-	color[1..3].each do |part|
-		num = part * 256
-		colorHex = "%x" % num
-		hex = hex + colorHex 
-	end
-	return "##{hex}"
 end
